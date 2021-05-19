13b74615e871441cf57076687c8f90cef0258c1b
David Fraser <davidfraser@Davids-MacBook-Air.local>
2021-05-19 20:02:15 +0100

    Added tests to verify tabs list can have a list of tabs. Added function to return the total cost of the rooms' tab

--- classes/bars.py
+++ classes/bars.py
@@ -20,3 +20,20 @@ class Bar:
     def start_tab(self, room):
         if not room.name in self.tab_list.keys():
             self.tab_list[room.name] = {}
+
+    def request_tab_cost(self, room):
+        cost = 0
+        for drink, amount in self.tab_list[room.name].items():
+            cost += drink.price * amount
+        return cost
+
+    # def pay_tab(self, room):
+    #     tab_cost = self.request_tab_cost(room)
+    #     group_cash = 0
+    #     for guest in room.guests:
+    #         group_cash += guest.wallet
+    #     if group_cash >= tab_cost:
+
+    #         return True
+    #     else:
+    #         return False
